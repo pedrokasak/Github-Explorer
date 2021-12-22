08e91fa731e17d5dcca3a6abddd7ce37b8d03669
Pedro Henrique <pedrohesm@gmail.com>
2021-12-22 11:50:02 -0300

    criando o primeiro componente e estilizando

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,17 @@
+function RepositoryList() {
+    return(
+        <section>
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                <li>
+                    <h3>Github</h3>
+                </li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+    )
+}
